c907ad5f634c5402268afdd30699cc562ef71512
0marSalah <omar.salah1597@gmail.com>
2024-02-23 22:36:10 +0200

    Add profile endpoint to retrieve user profile

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -66,5 +66,18 @@ def logout() -> str:
     return flask.redirect("/")
 
 
+@app.route("/profile", methods=["GET"], strict_slashes=False)
+def profile() -> str:
+    """GET /profile
+    Return:
+        - JSON string of the user profile.
+    """
+    session_id = flask.request.cookies.get("session_id")
+    user = AUTH.get_user_from_session_id(session_id)
+    if user is None:
+        flask.abort(403)
+    return flask.jsonify({"email": user.email}), 200
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
