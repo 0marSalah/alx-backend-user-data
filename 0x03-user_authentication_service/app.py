0dcf4abb93e926b42066feae35b841b281b4d190
0marSalah <omar.salah1597@gmail.com>
2024-02-23 22:29:52 +0200

    Refactor logout function in app.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -56,14 +56,13 @@ app.route('/sessions', methods=['DELETE'], strict_slashes=False)
 def logout() -> str:
     """ DELETE /sessions
     """
-    session_id = flask.request.cookies.get("session_id")
-
-    if session_id:
+    try:
+        session_id = flask.request.cookies.get("session_id")
         user = AUTH.get_user_from_session_id(session_id)
-        if user:
-            AUTH.destroy_session(user.id)
-            return flask.redirect("/")
-    flask.abort(403)
+        AUTH.destroy_session(user.id)
+        return flask.redirect("/")
+    except Exception:
+        flask.abort(403)
 
 
 if __name__ == "__main__":
