479e563b162ed41fe3251157ecc680984662ae8f
0marSalah <omar.salah1597@gmail.com>
2024-02-23 21:25:21 +0200

    Refactor password hashing function

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 """ Auth
 """
-from bcrypt import hashpw, gensalt
-from typing import Any
+import bcrypt
 
 
-def _hash_password(password: str) -> Any:
-    """ hash password
-    """
-    return hashpw(password.encode('utf-8'), gensalt())
+def _hash_password(password: str) -> bytes:
+    ''' a function that hashes password'''
+    salt = bcrypt.gensalt()
+    passWord = password
+
+    hashed_password = bcrypt.hashpw(passWord.encode('utf-8'), salt)
+    return hashed_password
